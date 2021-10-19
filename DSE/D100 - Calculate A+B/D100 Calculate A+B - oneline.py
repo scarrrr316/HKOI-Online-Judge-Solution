92a78e31891e37606e738932525919a3f8b9b6ce
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2021-10-20 00:12:16 +0800

    Create D100 Calculate A+B - oneline.py

---
+++ DSE/D100 - Calculate A+B/D100 Calculate A+B - oneline.py
@@ -0,0 +1,17 @@
+# same as https://github.com/scarrrr316/HKOI-Online-Judge-Solution/blob/main/HKOI/01024%20-%20HPA%20Addition/01024%20-%20HPA%20Addition%20-%20oneline.py
+
+print(int(input())+int(input()))
+
+# print(eval(input())+eval(input()))
+
+# print((lambda x,y:eval(x)+eval(y))(input(),input()))
+
+# print(sum(eval(input(),eval(input()))))
+
+# print(eval(input()+'+'+input()))
+
+# print(sum([eval(input()) for i in range(2)]))
+
+# print(sum(map(int,[input() for i in range(2)])))
+
+# print(eval(f'{input()}+{input()}'))
