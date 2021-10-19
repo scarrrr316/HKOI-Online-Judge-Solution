e51e236bce84efbcd3e1c96340be52f22ee516fa
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2021-10-19 23:51:39 +0800

    Create 01024 - HPA Addition - oneline.py

---
+++ HKOI/01024 - HPA Addition/01024 - HPA Addition - oneline.py
@@ -0,0 +1,15 @@
+print(int(input())+int(input()))
+
+# print(eval(input())+eval(input()))
+
+# print((lambda x,y:eval(x)+eval(y))(input(),input()))
+
+# print(sum(eval(input(),eval(input()))))
+
+# print(eval(input()+'+'+input()))
+
+# print(sum([eval(input()) for i in range(2)]))
+
+# print(sum(map(int,[input() for i in range(2)])))
+
+# print(eval(f'{input()}+{input()}'))
